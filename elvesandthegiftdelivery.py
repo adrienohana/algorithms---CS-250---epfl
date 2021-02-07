9dbf736224a7886b84f559d3671bc87c1a1e7950
adrienohana <61504470+adrienohana@users.noreply.github.com>
2021-02-07 10:20:37 +0100

    Update elvesandthegiftdelivery.py

--- elvesandthegiftdelivery.py
+++ elvesandthegiftdelivery.py
@@ -1,59 +1,91 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1561\cocoasubrtf600
-{\fonttbl\f0\fmodern\fcharset0 Courier-Bold;}
-{\colortbl;\red255\green255\blue255;\red0\green0\blue117;\red235\green236\blue237;}
-{\*\expandedcolortbl;;\cssrgb\c0\c0\c53333;\cssrgb\c93725\c94118\c94510;}
-{\*\listtable{\list\listtemplateid1\listhybrid{\listlevel\levelnfc23\levelnfcn23\leveljc0\leveljcn0\levelfollow0\levelstartat1\levelspace360\levelindent0{\*\levelmarker \{none\}.}{\leveltext\leveltemplateid1\'01.;}{\levelnumbers;}\fi-360\li720\lin720 }{\listname ;}\listid1}}
-{\*\listoverridetable{\listoverride\listid1\listoverridecount0\ls1}}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\tx220\tx720\pardeftab720\li720\fi-720\sl300\partightenfactor0
-\ls1\ilvl0
-\f0\b\fs26\fsmilli13200 \cf2 \cb3 import math\
-\
-#read n, m and k\
-s = input()\
-\
-line1 = list(map(int, s.split()))\
-if len(line1)==3:\
-    [n,m,k] = line1\
-else:\
-    print('Wrong input, try again')\
-\
-k_ = []\
-w_ji = []\
-h_ji = []\
-\
-for j in range(k):\
-    s = input()\
-    line = list(map(int, s.split()))\
-    k_.append(line[0])\
-    rest = line[1:]\
-    wj, hj = rest[0::2], rest[1::2]\
-    w_ji.append(wj)\
-    h_ji.append(hj)\
-    \
-k_, w_ji, h_ji\
-\
-c = [[0 for l in range(m+1)]for i in range(n+1)]\
-\
-def knapsack():\
-    for j in range(0,k):\
-        for i in range(1,k_[j]+1):\
-            if j != 0:\
-                sum_ = 0\
-                for h in range(j):\
-                    sum_ = sum_ + k_[h]\
-                index = i + sum_\
-            else:\
-                index = i\
-            for l in range(0,m+1):\
-                if l == 0:\
-                    c[index][l]  = 0\
-                elif w_ji[j][i-1] <= l:\
-                    c[index][l] = max(c[index-1][l],c[index-i][l-w_ji[j][i-1]]+h_ji[j][i-1])\
-                else:\
-                    c[index][l] = c[index-1][l]\
-knapsack()\
-print(str(c[n][m]) + "\\n")\
-\
-}
+import math
+import queue
+
+#read n, m and k
+s = input()
+
+line1 = list(map(int, s.split()))
+if len(line1)==2:
+    [n,m] = line1
+else:
+    print('Wrong input, try again')
+    
+    
+Edges = []
+for j in range(m):
+    s = input()
+    line = list(map(int, s.split()))
+    [a,b] = line
+    a -=1
+    b -=1
+    Edges.append([a,b])
+    
+adjacency_matrix = [[0 for i in range(n)] for j in range(n)]
+
+for edge in Edges:
+    [a,b] = edge
+    adjacency_matrix[a][b] +=1
+    adjacency_matrix[b][a] +=1
+    
+
+import queue
+import math
+
+class Graph: 
+
+    def __init__(self,adjacency_matrix): 
+        #adjacency matrix of capacity between vertices
+        self.graph = adjacency_matrix
+
+    def BFS(self,source, sink, parents):  
+        #create visited set
+        Q = queue.Queue()
+        visited = set()
+        
+        #enqueue source
+        Q.put(source) 
+        visited.add(source)
+        
+        while (Q.empty()==False): 
+            u = Q.get()
+            for ind, capacity in enumerate(self.graph[u]): 
+                if capacity > 0 and visited.isdisjoint({ind}) : 
+                    Q.put(ind)
+                    visited.add(ind)
+                    parents[ind] = u 
+                    if ind == sink:
+                        return True
+        return visited.isdisjoint({sink})==False
+    
+    def Ford_Fulkerson(self, source, sink):
+        max_flow = 0
+        parents = [-1]*n 
+        
+        while self.BFS(source, sink, parents) : 
+            aug_flow = math.inf
+            augmenting_path_flow = 0
+            u = sink 
+            while(u != source): 
+                #path flow is minimum edge capacity
+                aug_flow = min (aug_flow, self.graph[parents[u]][u]) 
+                u = parents[u]
+                
+            max_flow+=aug_flow
+            
+            u = sink
+            while(u != source):
+                self.graph[parents[u]][u] -= aug_flow
+                self.graph[u][parents[u]] -= aug_flow
+                self.graph[u][parents[u]] += aug_flow 
+                u = parents[u]
+                
+        return max_flow
+    
+G = Graph(adjacency_matrix)
+source = 0
+sink = n-1
+
+max_flow = G.Ford_Fulkerson(source, sink)
+
+            
+print(str(max_flow) + "\n")
